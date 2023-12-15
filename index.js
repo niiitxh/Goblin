f89e3324a9c77caced61db1f3c910cb3e6abf697
niiitxh <153849248+niiitxh@users.noreply.github.com>
2023-12-15 11:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.addEventListner('Click',() => {
+menu.addEventListner('click',() => {
     navlist.classList.toggle('open');
 })
 
