a73783146d5c4abaaa7c1058e4169b92eb9003df
niiitxh <153849248+niiitxh@users.noreply.github.com>
2023-12-15 11:00:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onClick = () => {
-    menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 };
 
